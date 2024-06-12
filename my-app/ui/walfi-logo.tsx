72d9d0ba3085eb78d0dc443335ae07c16a515e6c
sm9906 <suminkim0618@gmail.com>
2024-06-12 18:16:15 +0900

    [파일] 타입 폴더

--- my-app/ui/walfi-logo.tsx
+++ my-app/ui/walfi-logo.tsx
@@ -1,7 +1,7 @@
 export default function WalfiLogo() {
   return (
     <div
-      className={`items-center leading-none`}
+      className='items-center leading-none'
     >
       <p className='text-blue-700 text-[44px] font-bold select-none'>WALFI</p>
     </div>
