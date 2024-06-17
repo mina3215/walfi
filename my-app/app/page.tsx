4f0dd95b879ba4f557399ec2a5ed43bfa79c9fa5
박민아 <icherom@naver.com>
2024-06-17 23:28:39 +0900

    [기능] Recoil + ReactQuery 추가

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,40 +1,40 @@
-// "use client";
+"use client";
 
-// import { useQuery } from '@tanstack/react-query';
+import { useQuery } from '@tanstack/react-query';
 
-// interface User{
-//   userId: string,
-//   email?: string,
-//   namee?: string,
-//   birthDate?: string,
-//   phoneNumber?: string,
-//   userMainAccount?: string
-// }
-
-// async function getInfo() {
-//   return (
-//     await fetch("http://localhost:8094/user").then(
-//       (res) => res.json()
-//   ))as User[]
-// }
-
-// export default function Home() {
-//   const { data } = useQuery({
-//     queryKey: ["stream-hydrate-users"],
-//     queryFn: () => getInfo(),
-//     staleTime: 5*100,
-//   })
-//   console.log(data);
-
-//   return (
-//     <div>
-
-//     </div>
-//   );
-// }
-
-export default function Page() {
-  return <div>
-    <p>home1</p>
-  </div>
+interface User{
+  userId: string,
+  email?: string,
+  namee?: string,
+  birthDate?: string,
+  phoneNumber?: string,
+  userMainAccount?: string
 }
+
+async function getInfo() {
+  return (
+    await fetch("http://localhost:8094/user").then(
+      (res) => res.json()
+  ))as User[]
+}
+
+export default function Home() {
+  const { data } = useQuery({
+    queryKey: ["stream-hydrate-users"],
+    queryFn: () => getInfo(),
+    staleTime: 5*100,
+  })
+  console.log(data);
+
+  return (
+    <div>
+
+    </div>
+  );
+}
+
+// export default function Page() {
+//   return <div>
+//     <p>home1</p>
+//   </div>
+// }
--- my-app/utils/recoilRootProvider.tsx
+++ my-app/utils/recoilRootProvider.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { RecoilRoot } from 'recoil';
-// import Providers from '@/utils/provider';
+import Providers from '@/utils/provider';
 
 export default function RecoilRootProvider({
   children,
@@ -9,7 +9,7 @@ export default function RecoilRootProvider({
   children: React.ReactNode
 }) {
   return <RecoilRoot>
-    {children}
-    {/* <Providers>{children}</Providers> */}
+    {/* {children} */}
+    <Providers>{children}</Providers>
   </RecoilRoot>
 }
