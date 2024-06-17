9bbd9f485297e6316d71a9076db8573a2f2df38a
박민아 <icherom@naver.com>
2024-06-17 23:31:13 +0900

    [기능] cors test

--- backend/src/main/java/com/shinhan/walfi/controller/UserController.java
+++ backend/src/main/java/com/shinhan/walfi/controller/UserController.java
@@ -16,6 +16,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/user")
 @RequiredArgsConstructor
+@CrossOrigin(origins="http://localhost:3000")
 public class UserController {
 
     private final UserService userService;
